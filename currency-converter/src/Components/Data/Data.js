5fbbf48f1d51118f7925d162ef7cb061c031e8dd
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-06-13 23:21:03 +0200

    add dummy data

---
+++ currency-converter/src/Components/Data/Data.js
@@ -0,0 +1,43 @@
+const data = {
+  currencies: [
+    {
+      code: 'EUR',
+      sellRate: 1,
+      name: 'Euro',
+      sign: '€',
+    },
+
+    {
+      code: 'USD',
+      sellRate: 0.7041,
+      name: 'United States Dollars',
+      sign: '$',
+    },
+    {
+      code: 'TRY',
+      sellRate: 7.1,
+      name: 'Turkish Lira',
+      sign: '₺',
+    },
+    {
+      code: 'NZD',
+      sellRate: 0.9949,
+      name: 'New Zealand Dollars',
+      sign: '$',
+    },
+    {
+      code: 'JPY',
+      sellRate: 78.21,
+      name: 'Japanese Yen',
+      sign: '¥',
+    },
+    {
+      code: 'GBP',
+      sellRate: 0.5472,
+      name: 'British Pounds Sterling',
+      sign: '£',
+    },
+  ],
+};
+
+export default data;
