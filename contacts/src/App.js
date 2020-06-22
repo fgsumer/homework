ccdb1e3957d5405d4bf63429c669c0c255b68068
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-06-22 22:14:20 +0200

    add header component

---
+++ contacts/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hearder from './Components/Header';
+import Search from './Components/Search';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Hearder></Hearder>
+      <Search></Search>
+    </div>
+  );
+}
+
+export default App;
---
+++ contacts/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hearder() {
+  return (
+    <header>
+      <h3>Contacts</h3>
+      <h6>Create Contact</h6>
+    </header>
+  );
+}
+
+export default Hearder;
