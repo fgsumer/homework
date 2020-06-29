03c6477d15f9fce5ab6681f0ea9797560d9ae06f
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-06-29 15:04:53 +0200

    refactor: move state to App Component

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import Hearder from './Components/Header';
-import Search from './Components/Search';
+import Header from './Components/Header';
+import SearchContacts from './Components/SearchContacts';
+import ContactDetails from './Components/ContactDetails';
+
+import contacts from './assests/MockData.js';
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Hearder></Hearder>
-      <Search></Search>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // persons: contacts,
+      query: '',
+    };
+    this.searchHandler = this.searchHandler.bind(this);
+  }
+  searchHandler(event) {
+    this.setState({
+      query: event.target.value,
+    });
+  }
+  render() {
+    const { query } = this.state;
+
+    let filteredPersons = contacts.filter(
+      person =>
+        person.firstName.toLowerCase().indexOf(query.toLowerCase()) !== -1 ||
+        person.lastName.toLowerCase().indexOf(query.toLowerCase()) !== -1,
+    );
+
+    return (
+      <div>
+        <Header />
+        <div className="main">
+          <SearchContacts
+            searchHandler={this.searchHandler}
+            query={query}
+            filteredPersons={filteredPersons}
+          />
+          <ContactDetails filteredPersons={filteredPersons} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
