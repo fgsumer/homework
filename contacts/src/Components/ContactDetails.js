3e1e40dc3b5d05c22529d5613df6ec10588ffe20
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-06-29 22:58:44 +0200

    update contactDetails component

--- contacts/src/Components/ContactDetails.js
+++ contacts/src/Components/ContactDetails.js
@@ -2,17 +2,50 @@ import React from 'react';
 
 function ContactDetails({ filteredPersons }) {
   const mapped = filteredPersons.map(person => (
-    <div style={{ color: 'red' }} key={person.id}>
-      {person.firstName} <span style={{ fontWeight: 'bold' }}>{person.lastName}</span>
+    <div className="contact_details" style={{ color: 'red' }} key={person.id}>
+      <div className="contact_name">
+        {/* <img
+          className="icon"
+          alt="avatar"
+          src={`./profiles/${person.profileImage}`}
+          // src={'./profiles/female-1.jpg'}
+        /> */}
+        {/* <p>
+          {person.firstName} <span style={{ fontWeight: 'bold' }}>{person.lastName}</span>
+        </p> */}
+      </div>
+      <div className="row">
+        <div className="column">
+          <img
+            className="icon"
+            alt="avatar"
+            src={`./profiles/${person.profileImage}`}
+            // src={'./profiles/female-1.jpg'}
+          />
+        </div>
+        <div className="column">
+          <p style={{ fontSize: '3rem' }}>
+            {person.firstName} <span style={{ fontWeight: 'bold' }}>{person.lastName}</span>
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="column">
+          <p>MOBILE</p>
+          <p> TELEPHONE</p>
+          <p> EMAIL</p>
+          <p> HOME ADRESS</p>
+        </div>
+        <div className="column">
+          <p>{person.mobile}</p>
+          <p> {person.telephone}</p>
+          <p> {person.email}</p>
+          <p> {person.homeAddress}</p>
+        </div>
+      </div>
     </div>
   ));
-  return (
-    <>
-      <h1>Hello </h1>
-      <div>{mapped[0]}</div>
-      <div></div>
-    </>
-  );
+  return <>{mapped[0]}</>;
 }
 
 export default ContactDetails;
