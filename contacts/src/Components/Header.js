813e613b35f4544b63bd20edaaf4bf69a8bf7038
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-06-29 23:08:21 +0200

    update stylinh via flexbox

--- contacts/src/Components/Header.js
+++ contacts/src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header style={{ background: '#323142', display: 'flex', flexDirection: 'row' }}>
+    <header className="header">
       <h3>Contacts</h3>
       <h6>Create Contact</h6>
     </header>
