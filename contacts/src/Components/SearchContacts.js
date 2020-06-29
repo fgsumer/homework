779c15e1fc7dbbf4f25f662098e3103eb60c47c9
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-06-29 22:58:22 +0200

    update  search contact component

--- contacts/src/Components/SearchContacts.js
+++ contacts/src/Components/SearchContacts.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
-function SearchContacts({ persons, searchHandler, query, mappedPerson, filteredPersons }) {
+function SearchContacts({ searchHandler, query, filteredPersons }) {
   return (
     <>
-      <div>
-        <input className="search" type="text" value={query} onChange={searchHandler}></input>
-        {filteredPersons.map(person => {
-          return <div>{person.firstName}</div>;
-        })}
+      <div className="contacts_list">
+        <input
+          className="search"
+          placeHolder="Search..."
+          type="text"
+          value={query}
+          onChange={searchHandler}
+        ></input>
+        {filteredPersons.map(person => (
+          <div className="list_items" key={person.id}>
+            <img
+              className="icon"
+              alt="avatar"
+              src={`./profiles/${person.profileImage}`}
+              // src={'./profiles/female-1.jpg'}
+            />
+            <p>
+              {person.firstName} <span style={{ fontWeight: 'bold' }}>{person.lastName}</span>
+            </p>
+          </div>
+        ))}
       </div>
     </>
   );
